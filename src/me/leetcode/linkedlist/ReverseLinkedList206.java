534d9b1797b200b48d90e6ff3b85b07e3ef43c02
AlbertRui <ZR_sxlx@163.com>
2018-02-05 00:09:23 +0800

    modified my solutions

--- src/me/leetcode/linkedlist/ReverseLinkedList206.java
+++ src/me/leetcode/linkedlist/ReverseLinkedList206.java
@@ -16,6 +16,11 @@ package me.leetcode.linkedlist;
  *
  */
 public class ReverseLinkedList206 {
+    /**
+     * 我的解答，使用迭代
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
 	ListNode res = null;
 	ListNode next = null;
@@ -28,10 +33,34 @@ public class ReverseLinkedList206 {
 	return res;
     }
     
+    /**
+     * 参考解答：使用递归
+     * The recursive version is slightly trickier and the key is to work backwards. 
+     * Assume that the rest of the list had already been reversed,now how do I reverse the front part? 
+     * Let's assume the list is: n1 → … → nk-1 → nk → nk+1 → … → nm → Ø
+     * 
+     * Assume from node nk+1 to nm had been reversed and you are at node nk.
+     * 
+     * n1 → … → nk-1 → nk → nk+1 ← … ← nm
+     * 
+     * We want nk+1’s next node to point to nk.
+     * 
+     * So,nk.next.next = nk;
+     * 
+     * Be very careful that n1's next must point to Ø. If you forget about this,
+     * your linked list has a cycle in it. This bug could be caught if you test
+     * your code with a linked list of size 2.
+     * 
+     * @param head
+     * @return
+     */
     public ListNode reverseListByRecursively(ListNode head) {
-	if(head != null && head.next != null) {
-	    
+	if (head == null || head.next == null) {
+	    return head;
 	}
-	return head;
+	ListNode point = reverseListByRecursively(head.next);
+	head.next.next = head;
+	head.next = null;
+	return point;
     }
 }
