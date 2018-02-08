d81229edf00cc6000d35b37b39bf09e21065bcd9
AlbertRui <ZR_sxlx@163.com>
2018-02-08 11:05:27 +0800

    modified my solutions

--- src/me/leetcode/linkedlist/AddTwoNumbersII445.java
+++ src/me/leetcode/linkedlist/AddTwoNumbersII445.java
@@ -24,20 +24,40 @@ package me.leetcode.linkedlist;
  */
 public class AddTwoNumbersII445 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-	ListNode result = new ListNode(0);
+	ListNode tmp = new ListNode(0);
+	ListNode res = tmp;
 	l1 = reverse(l1);
 	l2 = reverse(l2);
-	while(l1 != null || l2 != null) {
-	    int a = l1 == null?0:l1.val;
-	    int b = l2 == null?0:l2.val;
+	int flag = 0;
+	int val = 0;
+	while (l1 != null || l2 != null) {
+	    int a = l1 == null ? 0 : l1.val;
+	    int b = l2 == null ? 0 : l2.val;
+	    val = (a + b + flag) % 10;
+	    flag = (a + b + flag) / 10;
+	    tmp = tmp.next = new ListNode((val));
+	    if (l1 != null) {
+		l1 = l1.next;
+	    }
+	    if (l2 != null) {
+		l2 = l2.next;
+	    }
 	}
-	return result;
+	if(flag == 1) {
+	    tmp.next = new ListNode(flag);
+	}
+	return reverse(res.next);
     }
-    
+
+    /**
+     * 反转链表
+     * @param head
+     * @return
+     */
     public ListNode reverse(ListNode head) {
 	ListNode next = null;
 	ListNode result = null;
-	while(head != null) {
+	while (head != null) {
 	    next = head.next;
 	    head.next = result;
 	    result = head;
