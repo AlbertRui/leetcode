9c795cc109c730504b351a71d00a25541e1880f6
AlbertRui <ZR_sxlx@163.com>
2018-03-15 23:45:09 +0800

    add my solutions

--- src/me/leetcode/linkedlist/ConvertSortedListtoBinarySearchTree109.java
+++ src/me/leetcode/linkedlist/ConvertSortedListtoBinarySearchTree109.java
@@ -3,6 +3,9 @@ package me.leetcode.linkedlist;
 import me.leetcode.util.ListNode;
 import me.leetcode.util.TreeNode;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 /**
  * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
  * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
@@ -21,5 +24,43 @@ import me.leetcode.util.TreeNode;
 public class ConvertSortedListtoBinarySearchTree109 {
     public TreeNode sortedListToBST(ListNode head) {
         return null;
+    }
+    /**
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        TreeNode root = null;
+        TreeNode p = root;
+        Stack<TreeNode> stack = new Stack<>();
+        for (int i = 0; i < preorder.length; i++) {
+            int temp = map.get(preorder[i]);
+            TreeNode node = new TreeNode(preorder[i]);
+            if (stack.isEmpty()) {
+                root = node;
+//              stack.add(node);
+                p = root;
+            } else {
+                if (temp < map.get(stack.peek().val)) {
+                    p.left = node;
+                    p = p.left;
+                } else {
+                    while (!stack.isEmpty() && temp > map.get(stack.peek().val)) {
+                        p = stack.pop();
+                    }
+                    p.right = node;
+                    p = p.right;
+                }
+            }
+            stack.add(node);
+        }
+
+        return root;
     }
 }
