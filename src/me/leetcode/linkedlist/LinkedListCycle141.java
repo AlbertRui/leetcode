0b1438a89263ff6b65c0c061637e82950e2a8545
AlbertRui <isruizhang@gmail.com>
2018-04-05 10:08:51 +0800

    add my solutions

--- src/me/leetcode/linkedlist/LinkedListCycle141.java
+++ src/me/leetcode/linkedlist/LinkedListCycle141.java
@@ -2,6 +2,9 @@ package me.leetcode.linkedlist;
 
 import me.leetcode.util.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a linked list, determine if it has a cycle in it.
  * Follow up:
@@ -12,6 +15,25 @@ import me.leetcode.util.ListNode;
  */
 public class LinkedListCycle141 {
     public boolean hasCycle(ListNode head) {
-        return true;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public boolean hasCycleBySet(ListNode head) {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
     }
 }
