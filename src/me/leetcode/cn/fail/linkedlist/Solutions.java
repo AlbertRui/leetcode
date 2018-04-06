0d82929f5b0eb44de180cc259d07de93c291c7f7
AlbertRui <isruizhang@gmail.com>
2018-04-06 20:27:56 +0800

    add my solutions

---
+++ src/me/leetcode/cn/fail/linkedlist/Solutions.java
@@ -0,0 +1,51 @@
+package me.leetcode.cn.linkedlist;
+
+import me.leetcode.util.ListNode;
+
+/**
+ * @author AlbertRui
+ * @date 2018-04-05 22:58
+ */
+public class Solutions {
+
+    /**
+     * 给定两个非空链表来代表两个非负整数，位数按照逆序方式存储，
+     * 它们的每个节点只存储单个数字。将这两数相加会返回一个新的链表。
+     * <p>
+     * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        long num1 = 0;
+        long num2 = 0;
+
+        int i = 0;
+        while (l1 != null) {
+            num1 += l1.val * Math.pow(10, i++);
+            l1 = l1.next;
+        }
+        i = 0;
+        while (l2 != null) {
+            num2 += l2.val * Math.pow(10, i++);
+            l2 = l2.next;
+        }
+        Long num = num1 + num2;
+        String numStr = num.toString();
+        ListNode result = new ListNode(0);
+        ListNode temp = result;
+        int j = numStr.length();
+        while (j > 0) {
+            temp.val = Integer.parseInt(String.valueOf(numStr.charAt(--j)));
+            if (j != 0) {
+                temp.next = new ListNode(0);
+                temp = temp.next;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/me/leetcode/cn/string/Solutions.java
@@ -0,0 +1,39 @@
+package me.leetcode.cn.fail.string;
+
+/**
+ * @author AlbertRui
+ * @date 2018-04-06 10:08
+ */
+public class Solutions {
+
+    /**
+     * 第二题
+     * 给定一个字符串，找出不含有重复字符的 最长子串 的长度。
+     * <p>
+     * 示例：
+     * <p>
+     * 给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
+     * <p>
+     * 给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
+     * <p>
+     * 给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列 而不是子串。
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int count = 1;
+        int max = 0;
+        int flag = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!s.substring(flag, i).contains(String.valueOf(s.charAt(i))))
+                count++;
+            else if (count > max) {
+                max = count;
+                count = 0;
+                flag = i;
+            }
+        }
+        return max;
+    }
+}
