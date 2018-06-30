60082fc38ec413517a40f4017cece440c5bf79e6
AlbertRui <isruizhang@gmail.com>
2018-06-30 23:57:30 +0800

    add my solutions

--- src/me/leetcode/cn/bt/Solutions.java
+++ src/me/leetcode/cn/bt/Solutions.java
@@ -223,4 +223,31 @@ public class Solutions {
         return isExist;
     }
 
+    /**
+     * 93. 复原IP地址
+     * 给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+     * 示例:
+     * 输入: "25525511135"
+     * 输出: ["255.255.11.135", "255.255.111.35"]
+     */
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        helper(s, res, 0, "", 0);
+        return res;
+    }
+
+    private void helper(String s, List<String> res, int index, String ret, int count) {
+        if (count > 4) return;
+        if (count == 4 && index == s.length()) {
+            res.add(ret);
+            return;
+        }
+        for (int i = 1; i < 4; i++) {
+            if (index + i > s.length()) break;
+            String temp = s.substring(index, index + i);
+            if((temp.startsWith("0") && temp.length() > 1) || (i == 3 && Integer.parseInt(temp) > 255)) continue;
+            helper(s, res, index + i, ret + temp + (count == 3 ? "" : "."), count + 1);
+        }
+    }
+
 }
